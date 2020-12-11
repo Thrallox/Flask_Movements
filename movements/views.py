1a48ce26513217836f59503c397282ab21075d76
Thrallox <javieraguilargene@gmail.com>
2020-12-11 16:38:36 +0100

    basededatos

--- movements/views.py
+++ movements/views.py
@@ -1,13 +1,64 @@
 from movements import app
-from flask import render_template
-
-import random
+from flask import render_template, request, url_for, redirect
+import csv
+import sqlite3
 
 @app.route('/')
-def listaMovimientos():
-    texto= 'Hola'
-    return render_template("movementsList.html",variable=texto )
+def listaIngresos():
+    conn = sqlite3.connect('movements/data/basededatos.db')
+    c = conn.cursor()
 
-@app.route('/dado')
-def tiradado():
-    return render_template('dado.html', tirada=random.randrange(1,7))
+    c.execute('SELECT fecha, concepto, cantidad, id FROM movimientos;')
+
+
+    '''
+    fIngresos = open("movements/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
+    ingresos = list(csvReader)
+    '''
+    ingresos = c.fetchall()
+
+    total = 0
+    for ingreso in ingresos:
+        total += float(ingreso[2])
+
+    conn.close()
+
+    return render_template("movementsList.html",datos=ingresos, total=total)
+
+@app.route('/creaalta', methods=['GET', 'POST'])
+def nuevoIngreso():
+    if request.method == 'POST':
+        # iNSERT INTO movimientos (cantidad, concepto, fecha) VALUES (1500, "Paga extra", "2020-12-16" )
+
+        conn = sqlite3.connect('movements/data/basededatos.db')
+        c = conn.cursor()
+
+        c.execute('INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?, ? ,? );', 
+                 (
+                    float(request.form.get('cantidad')),
+                    request.form.get('concepto'),
+                    request.form.get('fecha')
+                 )
+        )
+
+        conn.commit()
+        conn.close()
+        '''   
+        fIngresos = open("movements/data/basededatos.csv", "a", newline="")
+        csvWriter = csv.writer(fIngresos, delimiter=',', quotechar='"')
+        csvWriter.writerow([request.form.get('fecha'), request.form.get('concepto'), request.form.get('cantidad')])
+        '''
+        return redirect(url_for('listaIngresos'))
+        
+
+
+    return render_template("alta.html")
+
+
+@app.route("/modifica/<id>", methods=['GET', 'POST'])
+def modificaIngreso(id):
+    '''
+        1. Consulta el movimiento por id 
+        2. render_template(modifica.html, moviento=el resultado de la consulta anterior"
+    '''
--- yanomellamo_run.py
+++ yanomellamo_run.py
@@ -1,4 +1,4 @@
-from movements import app 
+from movements import app
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
